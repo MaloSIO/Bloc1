748a077562e327661ee1e0178f4345f35f795918
MaloSIO <malo.devic@sts-sio-caen.info>
2023-11-03 11:48:14 +0100

    Added and Modified

--- docs/assets/js/td4/td4.js
+++ docs/assets/js/td4/td4.js
@@ -8,5 +8,24 @@ const KeyPressFunction=function(e) {
 };
 
 document.addEventListener("DOMContentLoaded", function() {
-qs('#NameInput').addEventListener("keyup", KeyPressFunction);
+    AEL("#NameInput","keyup", KeyPressFunction);
+    AEL("#Créer","click",()=>{
+        const li=document.createElement("li");
+        li.innerText=qs("#elm").value;
+        li.style.color=qs("#Color").value;
+        qs("#ul-elms").appendChild(li);
+    });
+});
+
+// Récupérez l'élément d'entrée de couleur et le bouton "Add Background"
+const bgColorInput = document.getElementById("bg-color");
+const addBgButton = document.getElementById("Add Background");
+
+// Ajoutez un gestionnaire d'événement pour le bouton "Add Background"
+addBgButton.addEventListener("click", function() {
+  // Récupérez la couleur sélectionnée
+  const selectedColor = bgColorInput.value;
+
+  // Changez la couleur de fond du corps de la page
+  document.body.style.backgroundColor = selectedColor;
 });
