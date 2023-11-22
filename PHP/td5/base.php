9ca03489bbffe88e446d87124521b21404ba01c8
MaloSIO <malo.devic@sts-sio-caen.info>
2023-11-22 14:48:07 +0100

    modified

--- PHP/td5/base.php
+++ PHP/td5/base.php
@@ -1,3 +1,23 @@
 <?php
-    $message="You can't change it";
+    $message=$_GET['message']??"Message par défaut";
+    $nb=$_GET['nb']??1;
+    $i=0;
+    while($i<$nb){
         echo "<h1>$message</h1>";
+        $i++;
+    }
+?>
+<ul>
+    <li>
+        <a href= "?message=Hola">Salut en espagnol</a>
+    </li>
+    <li>
+        <a href= "?message=Hello">Salut en anglais</a>
+    </li>
+    <li>
+        <a href= "?message=Ciao">Salut en italien</a>
+    </li>
+    <li>
+        <a href="?message=Salut">Salut en français</a>
+    </li>
+</ul>
