7da4cb7fb4df8c903b6ae64a40ab97b6509022cc
MaloSIO <malo.devic@sts-sio-caen.info>
2023-11-22 16:50:57 +0100

    modified

--- PHP/td5/font-tester-form.php
+++ PHP/td5/font-tester-form.php
@@ -1,78 +1,23 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Display Variables</title>
-    <style>
-        #displayDiv {
-            margin-top: 20px;
-            padding: 10px;
-            border: 1px solid #ccc;
-            display: inline-block;
-        }
-    </style>
-</head>
-<body>
-
 <?php
-// Initialiser les valeurs par défaut ou à partir de la requête
-$message = isset($_GET['message']) ? $_GET['message'] : 'Hello, World!';
-$font = isset($_GET['font']) ? $_GET['font'] : 'Arial';
-$color = isset($_GET['color']) ? $_GET['color'] : '#000000';
-
-// Liste de polices
-$fonts = array(
-    'Arial', 'Verdana', 'Helvetica', 'Times New Roman', 'Courier New', 'Georgia',
-    'Palatino Linotype', 'Trebuchet MS', 'Book Antiqua', 'Impact', 'Tahoma',
-    'Geneva', 'Comic Sans MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Arial Black',
-    'Gadget', 'MS Sans Serif', 'MS Serif'
-);
+$message=$_GET['message']??'Default';
+$color=$_GET['color']??'black';
+$size=$_GET['size']??'12';
 ?>
-
-<form>
-    <label for="message">Message:</label>
-    <input type="text" id="message" name="message" value="<?php echo $message; ?>">
-    <br>
-    <br>
-    <label for="font">Font:</label>
-    <select id="font" name="font">
-        <?php
-        foreach ($fonts as $fontOption) {
-            echo "<option value=\"$fontOption\"";
-            if ($fontOption == $font) {
-                echo " selected";
-            }
-            echo ">$fontOption</option>";
-        }
-        ?>
-    </select>
-    <br>
-    <br>
-    <label for="color">Color:</label>
-    <input type="color" id="color" name="color" value="<?php echo $color; ?>">
-    <br>
-    <br>
-    <button type="button" onclick="updateDisplay()">Update Display</button>
-</form>
-
-<div id="displayDiv" style="font-family: <?php echo $font; ?>;
-            color: <?php echo $color; ?>;">
-    <?php echo $message; ?>
-</div>
-
-<script>
-    function updateDisplay() {
-        // Mettre à jour l'aperçu en direct
-        var message = document.getElementById('message').value;
-        var font = document.getElementById('font').value;
-        var color = document.getElementById('color').value;
-
-        var displayDiv = document.getElementById('displayDiv');
-        displayDiv.innerHTML = message;
-        displayDiv.style.fontFamily = font;
-        displayDiv.style.color = color;
-    }
-</script>
-</body>
-</html>
+<doctype !HTML>
+    <html lang="fr">
+        <head>
+            <meta charset="UTF-8">
+            <title>Font Tester</title>
+        </head>
+        <body>
+            <form>
+                <input type="number" name="size">
+                <input type="color" name="color">
+                <br>
+                <br>
+                <textarea name="message"></textarea>
+                <button type="submit">Submit</button>
+            </form>
+            <div style="color: <?=$color?>;font-size: <?=$size?>px"><?=$message?></div>
+        </body>
+    </html>
